7be1517c653a97513d8f3f49bcc37bddd10b7f41
Isabelle <belleyuri@gmail.com>
2025-01-06 13:35:53 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
-    images: {
-      domains: ['your-image-domain.com'],
-    },
-    env: {
-      NEXT_PUBLIC_API_BASE_URL: process.env.NEXT_PUBLIC_API_BASE_URL,
-    },
-  }
-  
-  module.exports = nextConfig
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+module.exports = nextConfig
