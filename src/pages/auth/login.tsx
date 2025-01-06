2d33f61f32cd22f5e52d492f60256bda7664ed3a
Isabelle <belleyuri@gmail.com>
2025-01-06 14:11:10 +0900

    Update login.tsx

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -4,9 +4,3 @@ export default function LoginPage() {
   return <LoginForm />;
 }
 
-// src/pages/auth/register.tsx
-import { RegisterForm } from '@/components/auth/RegisterForm';
-
-export default function RegisterPage() {
-  return <RegisterForm />;
-}
