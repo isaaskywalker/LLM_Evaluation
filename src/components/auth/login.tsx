e1b3128abe7bcc1919f2a1a9c06fa2866f8e3e15
Isabelle <belleyuri@gmail.com>
2025-01-06 13:58:00 +0900

    Create login.tsx

---
+++ src/components/auth/login.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from '@/components/auth/LoginForm';
+
+export default function LoginPage() {
+  return <LoginForm />;
+}
+
+// src/pages/auth/register.tsx
+import { RegisterForm } from '@/components/auth/RegisterForm';
+
+export default function RegisterPage() {
+  return <RegisterForm />;
+}
